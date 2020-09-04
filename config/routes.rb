61aa4ee570eb7e589c809e1b51b3829e4d724945
vagrant <vagrant@localhost.localdomain>
2020-09-04 06:42:54 +0000

    [いいね作成(大枠)]

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'home/about', to: 'home#about'
   resources :users, only: [:show, :edit, :update]
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create,:show]
   resources :products, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
     resources :comments, only: [:create, :destroy]
+    resources :favorites, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
