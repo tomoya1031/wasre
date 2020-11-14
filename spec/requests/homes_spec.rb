e7e1c0bc3ff0b74ac528f7be4f42a36f3a57be87
vagrant <vagrant@localhost.localdomain>
2020-11-14 15:04:51 +0000

    RSpec追加

--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -1,24 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe '会員新規登録ページ' do
-    context "会員新規登録ページが正しく表示される" do
+RSpec.describe "Homes", type: :request do
+  describe "ホーム画面" do
+    context "ホーム画面が正しく表示される" do
       before do
-        get new_user_registration_path
+        get get root_path
       end
       it 'リクエストは200 OKとなること' do
         expect(response.status).to eq 200
       end
     end
 
-    context "会員ログインページが正しく表示される" do
+    context "アバウト画面が正しく表示される" do
       before do
-        get new_user_session_path
+        get get about_path
       end
       it 'リクエストは200 OKとなること' do
         expect(response.status).to eq 200
       end
     end
-
   end
 end
