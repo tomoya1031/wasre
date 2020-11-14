f2c87066b56bf8bdd83c243ea8554d27edf4d7c7
vagrant <vagrant@localhost.localdomain>
2020-11-14 06:57:32 +0000

    RSpec追加

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+
+    before do
+      @comment = build(:comment)
+    end
+  
+    describe 'バリデーション' do
+      
+      it "全て入力してあるので保存される" do
+        expect(@comment.valid?).to eq(true)
+      end
+
+      it "コメントが入力されていないので保存されない" do
+        @comment.comment = ''
+        expect(@comment.valid?).to eq(false)
+      end
+
+      it "コメントが15文字を超えているので保存されない" do
+        @comment.comment = '1' * 301
+        expect(@comment.valid?).to eq(false)
+      end
+
+    end
+
+end
