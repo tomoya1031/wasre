9bc1bedf1c0780c2e7b637d0a2cfaa5c96ce22a2
vagrant <vagrant@localhost.localdomain>
2020-11-23 15:03:39 +0000

    RSpec追加

--- spec/models/sn_spec.rb
+++ spec/models/sn_spec.rb
@@ -1,5 +1,16 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Sn, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Sn, type: :model do
+    describe 'アソシエーションのテスト' do
+        let(:association) do
+          described_class.reflect_on_association(target)
+        end
+    
+        context '会員モデルとの関係' do
+          let(:target) { :user }
+          it 'N:1となっている' do
+            expect(association.macro).to eq :belongs_to
+          end
+        end
+      end
+end
